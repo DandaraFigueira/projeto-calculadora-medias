1918a2c539fa85a3ab0cfdad508c5bbd66d4e458
Dandara Figueira <dandarafigueira@hotmail.com>
2024-01-22 18:00:32 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,64 +1,48 @@
-const form = document.getElementById('form-atividade');
-const imgAprovado = '<img src="./imagens/aprovado.png" alt="Emoji celebrando" />';
-const imgReprovado = '<img src="./imagens/reprovado.png" alt="Emoji decepcionado" />';
-const atividades = [];
-const notas = [];
-const spanAprovado = '<span class="resultado aprovado">Aprovado</apan>';
-const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-const notaMinima = parseFloat(prompt("Digite a nota minima:"));
+function adicionarContato() {
+    //pegando oa valores do input
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var tel = document.getElementById("tel").value;
 
-let linhas = '';
+    //pegando a tabela e adicionando uma nova linha
+    var tabela = document.getElementById("tabelaContatos");
+    var novaLinha = tabela.insertRow();
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+    var celulaNome = novaLinha.insertCell(0);
+    var celulaEmail = novaLinha.insertCell(1);
+    var celulaTel = novaLinha.insertCell(2);
 
-    adicionaLinha();
-    atualizaTabela();
-    atualizaMediaFinal();
-});
+    //Criando nova linha para um checkbox
+    var celulaCheckbox = novaLinha.insertCell(3); // criando coluna de seleção
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    celulaCheckbox.appendChild(checkbox);
 
-function adicionaLinha() {
-    const inputNomeAtividade = document.getElementById('nome-atividade');
-    const inputNotaAtividade = document.getElementById('nota-atividade');
+    celulaNome.innerHTML = nome;
+    celulaEmail.innerHTML = email;
+    celulaTel.innerHTML = tel;
 
-    if (atividades.includes(inputNomeAtividade.value)) {
-        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
-    } else {
-        atividades.push(inputNomeAtividade.value0);
-        notas.push(parseFloat(inputNotaAtividade.value));
-
-        let linha = '<tr>';
-        linha += `<td>${inputNomeAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
-        linha += `</tr>`;
-    
-        linhas +=linha;
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("tel").value = "";
     }
 
-    inputNomeAtividade.value = '';
-    inputNotaAtividade.value = '';
-}
+    //Função Excluir contato
+    function excluirContato() {
+        //Pegar tabela
+        var tabela = document.getElementById("tabelaContatos");
+        //Pegar checkbox
+        var checkboxes = tabela.querySelectorAll("input[type='checkbox']");
 
-    function atualizaTabela() {
-        const corpoTabela = document.querySelector('tbody');
-        corpoTabela.innerHTML = linhas;
-}
-
-function atualizaMediaFinal() {
-    const mediaFinal = calculaMediaFinal();
-
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
-
-}
-
-function calculaMediaFinal() {
-    let somaDasNotas =0;
-
-    for (let i=0; i <notas.length; i++) {
-        somaDasNotas +=notas [i];
+        //Criar condição e chamar função "confirm" para abrir caixa de dialogo
+        if (confirm("Tem certeza de que deseja excluir os contatos selecionados?")) { 
+            //Iniciar loop verificando do final até o primeiro checkbox
+            for (var i = checkboxes.length - 1; i >= 0; i--) {
+                //Verifica se o elemento atual na array é um checkbox e se ele esta marcado
+                if (checkboxes[i].type === "checkbox" && checkboxes[i].checked) {
+                    //Exclui linha 
+                    tabela.deleteRow(i);
+                }
+            }
+        }
     }
-
-    return somaDasNotas / notas.length;
-}
